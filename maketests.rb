7782066e0ab18037d85a1abda11b6d4479724b30
James Tucker <jftucker@gmail.com>
2014-07-21 15:35:36 -0700

    Add Apache headers to each file, per guidelines

--- maketests.rb
+++ maketests.rb
@@ -1,3 +1,17 @@
+# Copyright 2014 Google Inc. All rights reserved.
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # TODO(raggi): reach out to masinter@adobe.com and plh@w3.org and get this party started.
 require 'fileutils'
 require 'uri'
@@ -130,7 +144,27 @@ fieldsets.each do |name, set|
 end
 
 open('index.html', 'w') do |io|
-  io.puts "<!doctype html><html><head><title>multifail</title></head><body><ul>"
+  io.puts "<!doctype html>"
+  io.puts <<-HTML
+<!--
+Copyright 2014 Google Inc. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+-->
+  HTML
+  
+  
+  io.puts "<html><head><title>multifail</title></head><body><ul>"
   Dir['tests/*.html'].each do |test|
     io.puts "<li><a href='#{URI.escape test}'>#{test}</a></li>"
   end
